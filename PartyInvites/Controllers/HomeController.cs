08a1294b2ee92b68969b9531851652cc8cf39879
cgriffin902 <cgriffin.902@gmail.com>
2015-03-20 22:17:49 -0500

    first

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good After Noon";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //ToDo Email the response to organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "please Enter Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter Email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage= "Please Enter a Valid Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please Enter a Phone Number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please Select one")]
+        public bool? WillAttend { get; set; }
+    }
+}
